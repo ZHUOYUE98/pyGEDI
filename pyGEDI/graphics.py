29d47925bd7bef5fdc6f32f9bd6f5afa1af10fed
ZhuoYue <939121816@qq.com>
2024-10-09 14:48:00 +0800

    修改

--- pyGEDI/graphics.py
+++ pyGEDI/graphics.py
@@ -23,16 +23,18 @@ def histRaster(file_TIFF,bins):
     gedi_dataset = gdal.Open(file_TIFF)
     gedi_raster = gedi_dataset.GetRasterBand(1)
     noDataVal = gedi_raster.GetNoDataValue();print(noDataVal)
-    scaleFactor = gedi_raster.GetScale()    
-    cols = gedi_dataset.RasterXSize; 
+    scaleFactor = gedi_raster.GetScale()
+    if scaleFactor is None:
+        scaleFactor = 1
+    cols = gedi_dataset.RasterXSize;
     rows = gedi_dataset.RasterYSize;
-    gedi_array = gedi_dataset.GetRasterBand(1).ReadAsArray(0,0,cols,rows).astype(np.float)
+    gedi_array = gedi_dataset.GetRasterBand(1).ReadAsArray(0,0,cols,rows).astype(float)
     gedi_array[gedi_array==int(noDataVal)]=np.nan
     gedi_array=gedi_array/scaleFactor
     gedi_nonan_array = copy.copy(gedi_array)
-    gedi_nonan_array = gedi_nonan_array[~np.isnan(gedi_array)]    
+    gedi_nonan_array = gedi_nonan_array[~np.isnan(gedi_array)]
     fig,ax= plt.subplots(figsize=(5,5))
-    plt.hist(gedi_nonan_array.flatten(),bins=bins) 
+    plt.hist(gedi_nonan_array.flatten(),bins=bins)
     plt.title('Distribution of GEDI points')
     plt.xlabel('GEDI Tree Height (m)'); plt.ylabel('Number of pixels')
 
